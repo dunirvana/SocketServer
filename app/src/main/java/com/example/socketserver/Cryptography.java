386909949b49f0d52e8be24c6efee76dbbd178cf
Eduardo Teixeira <dunirvana@gmail.com>
2021-02-24 22:49:44 -0300

    Criptografar o conteudo trafegado (AES)

--- app/src/main/java/com/example/socketserver/Cryptography.java
+++ app/src/main/java/com/example/socketserver/Cryptography.java
@@ -16,7 +16,7 @@ public class Cryptography {
         byte[] byteMessage = nullPadString(message).getBytes();
         byte[] byteKey = key.getBytes();
         Key secretKey = new SecretKeySpec(byteKey, "AES");
-        Cipher c = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+        Cipher c = Cipher.getInstance("AES");
         c.init(Cipher.ENCRYPT_MODE, secretKey);
         byte[] cipher = c.doFinal(byteMessage);
 
@@ -30,7 +30,7 @@ public class Cryptography {
 
         byte[] byteKey = key.getBytes();
         Key secretKey = new SecretKeySpec(byteKey,"AES");
-        Cipher c = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+        Cipher c = Cipher.getInstance("AES");
         c.init(Cipher.DECRYPT_MODE, secretKey);
         byte[] byteDecoded = c.doFinal(toHex(decmessage));
 
